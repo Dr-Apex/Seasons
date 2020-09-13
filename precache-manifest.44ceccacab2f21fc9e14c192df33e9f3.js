3bcbc6472cee7bd4729321eb4c6e5a385f007436
Aayush Mahapatra <dr.aaaaaaaa8@gmail.com>
2020-09-13 17:56:58 +0530

    Updates

---
+++ precache-manifest.44ceccacab2f21fc9e14c192df33e9f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3febc279eee9615b6508946b710773b",
+    "url": "/Dr-Apex/Seasons/index.html"
+  },
+  {
+    "revision": "087e79aec6690799b285",
+    "url": "/Dr-Apex/Seasons/static/css/main.7f358f7d.chunk.css"
+  },
+  {
+    "revision": "a73a225fc1db1be69a4e",
+    "url": "/Dr-Apex/Seasons/static/js/2.cc535638.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Dr-Apex/Seasons/static/js/2.cc535638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087e79aec6690799b285",
+    "url": "/Dr-Apex/Seasons/static/js/main.2ba9735e.chunk.js"
+  },
+  {
+    "revision": "58f448f82a651a798214",
+    "url": "/Dr-Apex/Seasons/static/js/runtime-main.69c06d05.js"
+  }
+]);
